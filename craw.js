d8ae7526dd9d3f806dd340311ed9bc7159b6a632
nguyentrongbang <nguyentrongbang9x@gmail.com>
2015-08-23 21:02:00 +0700

    add .gitignore craw.js

---
+++ craw.js
@@ -0,0 +1,30 @@
+function contains(list, k) {
+  for(var i=0; i < list.length; i++){
+    if(list[i] === k){
+      return true;
+    }
+  }
+  return false;
+}
+
+
+var re = /(https?:\/\/teespring.com\/[a-zA-Z0-9-]+)/gmi; 
+var str = document.getElementById("contentArea").innerHTML;
+var m;
+var links = [];
+var count = 0; 
+while ((m = re.exec(str)) !== null) {
+    if (m.index === re.lastIndex) {
+        re.lastIndex++;
+    }
+    if (!contains(links, m[0])) {
+        count++;
+        links.push(m[0]);
+        console.log(count + ": " + m[0]);
+    }
+}
+
+
+document.getElementById("u_0_r").click()
+var re = /"(https?:\/\/teespring.com\/[^\s]+)"/gmi; 
+var re = /(https?:\/\/teespring.com\/[^\s]+)<\//gmi; 
